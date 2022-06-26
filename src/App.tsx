6c5b11036ee5647c74252e802a01d5ba874f4771
nominalrune <nominalrune.yokoyama@gmail.com>
2022-06-27 00:02:43 +0900

    gd

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Outlet
 } from "react-router-dom";
 import {
   QueryClient,
@@ -11,7 +12,10 @@ import {
 import './App.css';
 import { Base } from './components/Base';
 import { Login } from './components/Login';
-import {Register} from "./components/Register"
+import {Register} from "./components/Register";
+
+import { LogList } from 'objects/Logs/view/LogList';
+import { LogNew } from 'objects/Logs/view/LogNew';
 
 const queryClient = new QueryClient();
 
@@ -26,6 +30,15 @@ function App() {
           <Route path="/" element={<Base user={user} />}>
             <Route path="login" element={<Login set={setUser} />} />
             <Route path="register" element={<Register set={setUser} />} />
+            <Route path="log" element={<Outlet />} >
+            <Route path="log/" element={<LogList logs={
+              [
+                {title:"",detail:"",date:new Date(),timeFrom:new Date(),timeTo:new Date(),subject:""},
+                {title:"02",detail:"02",date:new Date(),timeFrom:new Date(),timeTo:new Date(),subject:""}
+              ]
+              } />} />
+            <Route path="new" element={<LogNew user={user} />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
